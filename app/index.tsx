d2b46d3586ff73cb9bf27ee99a00237a615fc629
zeyadsleem <zeyadsleem1@gmail.com>
2024-10-17 15:29:00 +0300

    Add a list empty component

--- app/index.tsx
+++ app/index.tsx
@@ -1,23 +1,16 @@
-import { useState } from "react";
-import { ScrollView, StyleSheet, TextInput } from "react-native";
-import { ShoppingListItem } from "../components/ShoppingListItem";
+import { StyleSheet, TextInput, FlatList, View, Text } from "react-native";
 import { theme } from "../theme";
+import { ShoppingListItem } from "../components/ShoppingListItem";
+import { useState } from "react";
 
 type ShoppingListItemType = {
   id: string;
   name: string;
 };
 
-const initialList: ShoppingListItemType[] = [
-  { id: "1", name: "Coffee" },
-  { id: "2", name: "Tea" },
-  { id: "3", name: "Milk" },
-];
-
 export default function App() {
-  const [shoppingList, setShoppingList] =
-    useState<ShoppingListItemType[]>(initialList);
-  const [value, setValue] = useState("");
+  const [shoppingList, setShoppingList] = useState<ShoppingListItemType[]>([]);
+  const [value, setValue] = useState<string>();
 
   const handleSubmit = () => {
     if (value) {
@@ -26,35 +19,40 @@ export default function App() {
         ...shoppingList,
       ];
       setShoppingList(newShoppingList);
-      setValue("");
+      setValue(undefined);
     }
   };
 
   return (
-    <ScrollView
+    <FlatList
+      data={shoppingList}
       style={styles.container}
       contentContainerStyle={styles.contentContainer}
       stickyHeaderIndices={[0]}
-    >
-      <TextInput
-        placeholder="E.g. Coffee"
-        style={styles.textInput}
-        value={value}
-        onChangeText={setValue}
-        returnKeyType="done"
-        onSubmitEditing={handleSubmit}
-      />
-      {shoppingList.map((item) => (
-        <ShoppingListItem name={item.name} key={item.id} />
-      ))}
-    </ScrollView>
+      ListHeaderComponent={
+        <TextInput
+          value={value}
+          style={styles.textInput}
+          onChangeText={setValue}
+          placeholder="E.g Coffee"
+          onSubmitEditing={handleSubmit}
+          returnKeyType="done"
+        />
+      }
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>Your shopping list is empty</Text>
+        </View>
+      }
+      renderItem={({ item }) => <ShoppingListItem name={item.name} />}
+    ></FlatList>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
     backgroundColor: theme.colorWhite,
+    flex: 1,
     paddingTop: 12,
   },
   contentContainer: {
@@ -64,10 +62,15 @@ const styles = StyleSheet.create({
     borderColor: theme.colorLightGrey,
     borderWidth: 2,
     padding: 12,
+    fontSize: 18,
+    borderRadius: 50,
     marginHorizontal: 12,
     marginBottom: 12,
-    fontSize: 18,
-    borderRadius: 15,
     backgroundColor: theme.colorWhite,
+  },
+  listEmptyContainer: {
+    justifyContent: "center",
+    alignItems: "center",
+    marginVertical: 18,
   },
 });
