4a9082c71f9dfee7512ee67d48073d101c0c9be2
zeyadsleem <zeyadsleem1@gmail.com>
2024-10-21 21:18:24 +0300

    Show past completions on the history page

--- app/counter/history.tsx
+++ app/counter/history.tsx
@@ -1,21 +1,67 @@
-import { View, Text, StyleSheet } from "react-native";
+import { format } from "date-fns";
+import { useEffect, useState } from "react";
+import { View, Text, StyleSheet, FlatList } from "react-native";
+import { countdownStorageKey, PersistedCountdownState } from ".";
+import { theme } from "../../theme";
+import { getFromStorage } from "../../utils/storage";
+
+const fullDateFormat = `LLL d yyyy, h:mm aaa`;
 
 export default function HistoryScreen() {
+  const [countdownState, setCountdownState] =
+    useState<PersistedCountdownState>();
+
+  useEffect(() => {
+    const init = async () => {
+      const value = await getFromStorage(countdownStorageKey);
+      setCountdownState(value);
+    };
+    init();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>History</Text>
-    </View>
+    <FlatList
+      style={styles.list}
+      contentContainerStyle={styles.contentContainer}
+      data={countdownState?.completedAtTimestamps}
+      ListEmptyComponent={
+        <View style={styles.listEmptyContainer}>
+          <Text>No history</Text>
+        </View>
+      }
+      renderItem={({ item }) => (
+        <View style={styles.listItem}>
+          <Text style={styles.lsitItemText}>
+            {format(item, fullDateFormat)}
+          </Text>
+        </View>
+      )}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  list: {
     flex: 1,
+    backgroundColor: theme.colorWhite,
+  },
+  contentContainer: {
+    marginTop: 8,
+  },
+  listItem: {
+    backgroundColor: theme.colorLightGrey,
+    alignItems: "center",
+    marginHorizontal: 8,
+    marginBottom: 8,
+    borderRadius: 6,
+    padding: 12,
+  },
+  lsitItemText: {
+    fontSize: 18,
+  },
+  listEmptyContainer: {
     justifyContent: "center",
     alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  text: {
-    fontSize: 24,
+    marginVertical: 18,
   },
 });
